515c1b243b7557a853b2a67c127838310bb122ea
pavithra <pavithraatcse@gmail.com>
2020-03-25 21:59:01 +0530

    Firts Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+app.get("/url", (req, res) => {
+  
+  res.send("Hello....:-)");
+   });
+app.listen(3000,() =>{
+    console.log("server is running on port 3000");
+})
